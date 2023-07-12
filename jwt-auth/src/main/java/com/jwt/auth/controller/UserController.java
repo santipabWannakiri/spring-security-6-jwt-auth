5a2c425742a4c7959956c9e6e541c1bcb74d0cce
Nokia3310 <santipab1991@gmail.com>
2023-07-13 00:54:14 +0700

    Added log to UserController

--- jwt-auth/src/main/java/com/jwt/auth/controller/UserController.java
+++ jwt-auth/src/main/java/com/jwt/auth/controller/UserController.java
@@ -55,16 +55,20 @@ public class UserController {
     public ResponseEntity<GenericResponse> registerUser(@RequestBody @Valid User userInfo, BindingResult result) {
         if (result.hasErrors()) {
             List<String> errorMessages = result.getFieldErrors().stream().map(error -> error.getField() + ": " + error.getDefaultMessage()).collect(Collectors.toList());
+            log.error("[/signup] --> INVALID_FORMAT  (\""+errorMessages.get(0)+"\")");
             throw new InvalidFormatException(errorMessages.get(0));
         } else {
             User existingUser = userService.findByUser(userInfo.getUsername());
             if (existingUser != null) {
+                log.error("[/signup] --> USER_NOT_FOUND  (User: \""+userInfo.getUsername()+"\" duplicated)");
                 throw new UserDuplicateException(USER_DUPLICATE_MESSAGE);
             } else {
                 User createdUser = userService.createUser(userInfo);
                 if (createdUser != null) {
+                    log.info("[/signup] --> SUCCESS  (User: \""+userInfo.getUsername()+"\" registered)");
                     return utilityService.responseSuccess(REGISTER_SUCCESS_MESSAGE);
                 } else {
+                    log.info("[/signup] --> UNABLE_REGISTER  (User: \""+userInfo.getUsername()+"\" unable register)");
                     throw new InternalErrorException(UNABLE_REGISTER_MESSAGE);
                 }
             }
@@ -76,13 +80,16 @@ public class UserController {
     public ResponseEntity<?> login(@RequestBody @Valid UserCredentials credentials, BindingResult result) {
         if (result.hasErrors()) {
             List<String> errorMessages = result.getFieldErrors().stream().map(error -> error.getField() + " : " + error.getDefaultMessage()).collect(Collectors.toList());
+            log.error("[/login] --> INVALID_FORMAT  (\""+errorMessages.get(0)+"\")");
             throw new InvalidFormatException(errorMessages.get(0));
         }
         User existingUser = userService.findByUser(credentials.getUsername());
         if (existingUser == null) {
+            log.error("[/login] --> USER_NOT_FOUND  (User: \""+credentials.getUsername()+"\" not found)");
             throw new UserNotFoundException(USER_NOT_FOUND_MESSAGE);
         }
         if (!passwordEncoder.matches(credentials.getPassword(), existingUser.getPassword())) {
+            log.error("[/login] --> PASSWORD_INCORRECT  (User: \""+credentials.getUsername()+"\" password incorrect)");
             throw new IncorrectPasswordException(PASSWORD_INCORRECT_MESSAGE);
         }
         return processUser(existingUser);
@@ -97,13 +104,16 @@ public class UserController {
         }
         Token refreshToken = tokenService.findRefreshToken(reqRefreshToken.getRefreshToken());
         if (refreshToken == null) {
+            log.error("[/refresh] --> REFRESH_TOKEN_NOT_FOUND  (Not found refresh token in payload)");
             throw new TokenNotFoundException(REFRESH_TOKEN_NOT_FOUND_MESSAGE);
         }
         if (tokenService.isTokenExpired(refreshToken.getExpirationDate())) {
+            log.error("[/refresh] --> REFRESH_TOKEN_EXPIRED  (The token : \""+refreshToken.getExpirationDate()+"\" expired)");
             throw new TokenExpiredException(REFRESH_TOKEN_EXPIRED_MESSAGE);
         }
         Optional<User> existingUser = userService.findById(refreshToken.getUser().getId());
         if (!existingUser.isPresent()) {
+            log.error("[/login] --> USER_NOT_FOUND  (User: \""+refreshToken.getUser()+"\" not found)");
             throw new UserNotFoundException(USER_NOT_FOUND_MESSAGE);
         }
         return processUser(existingUser.get());
@@ -113,14 +123,17 @@ public class UserController {
         List<String> roleList = user.getRoles().stream().map(Role::getName).collect(Collectors.toList());
         Map<String, Object> accessTokenObject = tokenService.generateToken(user.getUsername(), roleList);
         if (accessTokenObject == null) {
+            log.error("[processUser] --> UNABLE_CREATE_TOKEN  (Unable generate token)");
             throw new InternalErrorException(UNABLE_CREATE_TOKEN_MESSAGE);
         }
         Map<String, Object> refreshTokenObject = tokenService.generateRefreshToken();
         Token refreshToken = tokenService.recordToken(new Token((String) refreshTokenObject.get("refreshToken"), TokenType.REFRESH_TOKEN, TokenStatus.ACTIVE, (Date) refreshTokenObject.get("expiresIn"), (Date) refreshTokenObject.get("currentTime"), user));
         if (refreshToken == null) {
+            log.error("[processUser] --> UNABLE_CREATE_REFRESH_TOKEN  (Unable generate refresh token)");
             throw new InternalErrorException(UNABLE_CREATE_TOKEN_MESSAGE);
         }
         JwtData jwtData = new JwtData((String) accessTokenObject.get("token"), refreshToken.getTokenValue(), (Date) accessTokenObject.get("expiresIn"));
+        log.info("[processUser] --> SUCCESS_GENERATE_TOKEN  (Success generate token for user : \""+user.getUsername()+"\")");
         return utilityService.entityJwtTokenResponseMessage(HttpStatus.OK, new JwtTokenResponse(SUCCESS_CODE, SUCCESS_MESSAGE_CODE, jwtData));
     }
 }
